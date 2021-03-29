52f78266f496adc578a79bb73b81b286b72c31a0
Simone Bresci <simone.bresci.05@gmail.com>
2021-03-29 16:09:44 +0200

    creati controllers authors e post

---
+++ resources/views/authors/create.blade.php
@@ -0,0 +1,1 @@
+<h1>Authors create</h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// CRUD AUTHORS
+Route::resource('/authors', 'AuthorController');
+
+
+Route::get('/posts', 'PostController@index');
